cf7ecc66ad1735bfd5ce267f8c13f1d668044948
deepak-appinventiv <deepaknath.tiwari@appinventiv.com>
2025-01-08 18:12:58 +0530

    Deploy site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,14 +10,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://quivio_document.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/checkpoint_doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'deepak-appinventiv', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
